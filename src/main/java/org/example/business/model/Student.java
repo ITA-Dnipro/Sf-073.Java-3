27ad9618ac29ee7dc4ff9c1b53ae7b9757c33ece
penzata <penzata86@yahoo.com>
2022-12-17 09:01:26 +0200

    commit - entity class

--- src/main/java/org/example/business/model/Student.java
+++ src/main/java/org/example/business/model/Student.java
@@ -1,5 +1,46 @@
 package org.example.business.model;
 
+import org.example.persistence.annotations.Id;
+
+@Entity
+@Table(name = "students")
 public class Student {
-    //todo implement entity class
+
+    @Id
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+
+
+    public Student(String firstName) {
+        this.firstName = firstName;
+    }
+
+    private Student() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + firstName + '\'' +
+                '}';
+    }
 }
