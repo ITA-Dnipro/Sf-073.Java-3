73f3abff9b6ee80f9aacf53867d781c2d9d7a8aa
penzata <penzata86@yahoo.com>
2022-12-19 13:08:57 +0200

    commit - created table

--- src/test/java/org/example/persistence/ormanager/ORManagerTest.java
+++ src/test/java/org/example/persistence/ormanager/ORManagerTest.java
@@ -1,24 +1,55 @@
 package org.example.persistence.ormanager;
 
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.db.type.Source;
 import org.assertj.db.type.Table;
+import org.example.domain.model.Student;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.sql.DataSource;
 
+import java.sql.*;
+
+import static org.assertj.db.output.Outputs.output;
+
+@Slf4j
 class ORManagerTest {
+    private static final String STUDENTS_TABLE = """
+            CREATE TABLE students
+            (
+            id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+            first_name VARCHAR(30) NOT NULL
+            )
+            """;
     DataSource dataSource;
-    Source source;
+    static Source source;
+    static Connection conn;
+    Student student;
+    private static final String DB_URL = "jdbc:h2:mem:test";
+
+    @BeforeAll
+    static void setUp() throws SQLException {
+        conn = DriverManager.getConnection(DB_URL);
+        conn.prepareStatement(STUDENTS_TABLE).executeUpdate();
+        source = new Source("jdbc:h2:mem:test", "", "");
+    }
 
     @BeforeEach
     void init() {
-
-        source = new Source("jdbc:h2:mem:test", "sa", "");
+        student = new Student("Bob");
     }
 
     @Test
     void DBTesting() {
-        Table table = new Table(dataSource, "students");
+        Table table = new Table(source, "students");
+        output(table).toConsole();
+    }
+
+    @AfterAll
+    static void close() throws SQLException {
+        conn.close();
     }
 }
